29cf5da04b294688c28f1f168156b9c0379600b3
Mokenyu <mokenyujunior@gmail.com>
2026-03-27 21:09:10 -0400

    Changed: My email name under Azure monitor alerts system

--- storage.tf
+++ storage.tf
@@ -1,9 +1,9 @@
 resource "azurerm_storage_account" "app" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                       = var.storage_account_name
+  resource_group_name        = azurerm_resource_group.main.name
+  location                   = azurerm_resource_group.main.location
+  account_tier               = "Standard"
+  account_replication_type   = "LRS"
   https_traffic_only_enabled = true
   min_tls_version            = "TLS1_2"
   tags = {
